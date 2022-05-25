5d481b18ca64aed1a7c9e29fdfbc7bdcfae76013
ture9200 <didtjddms09@naver.com>
2022-05-25 17:50:46 +0900

    2022-05-25

---
+++ src/components/TodoBoard.js
@@ -0,0 +1,12 @@
+import React from "react"
+import {names} from "module"
+
+function TodoBoard() { 
+    return (
+        <div>
+          <h1>Todo list</h1>
+        </div>
+    )
+}
+
+export default TodoBoard
